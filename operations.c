56ee604af8636e757498bd53b3c18215c38f00ae
Duarte Serrano <duarte.f.m.serrano@tecnico.ulisboa.pt>
2024-12-11 16:16:29 +0000

    top10 portugal

--- operations.c
+++ operations.c
@@ -155,8 +155,8 @@ int kvs_backup(char *dirpath, int bck_count) {
     while (keyNode != NULL) {
       char *value = keyNode->value;
       char *key = keyNode->key;
-      size_t bck_len = strlen(value) + strlen(key) + 6;
-      char *bck = malloc(bck_len);
+      size_t bck_len = strlen(value) + strlen(key) + 5;
+      char *bck = malloc(bck_len + 1);
 
       sprintf(bck, "(%s, %s)\n", key, value);
 
